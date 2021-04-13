fd030b48066ddebab77f99a8625b3a924129e4e9
Carolina Ponce <caromponce@gmail.com>
2021-04-13 19:50:35 -0300

    navbar

---
+++ cinema-ponce/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './resNavBar/claqueta-8.png';
+import './NavBar.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+export default function NavBar() {
+    return (
+        <>
+            <Navbar className="navBar" variant="dark">
+                <Navbar.Brand className="brand" href="#home">
+                    <img
+                        src={logo}
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top"
+                        alt="cinema logo"
+                    />
+                    Cinema</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Películas</Nav.Link>
+                    <Nav.Link href="#features">Próximos Estrenos</Nav.Link>
+                    <Nav.Link href="#pricing">Salas</Nav.Link>
+                </Nav>
+            </Navbar>
+        </>
+    )
+}
